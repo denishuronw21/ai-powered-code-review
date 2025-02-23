c80e5c40be820fc11fcc4602b5609ed196453583
denishuronw21 <notbdenish@gmail.com>
2025-02-23 14:30:34 +0530

    Changes: Completed  Integration frontend with backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const aiRoutes = require("./routes/ai.routes")
+const cors = require("cors")
+
 const app =express()
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send("Hello World")   
---
+++ frontend/src/App.jsx
@@ -0,0 +1,76 @@
+import prism from "prismjs"
+import "prismjs/themes/prism-tomorrow.css"
+import Editor from "react-simple-code-editor"
+import './App.css'
+import { useEffect, useState } from "react"
+import Markdown from "react-markdown"
+import axios from "axios"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+
+function App() {
+  
+  const [code,setCode] = useState(` function sum(){
+                return 1 + 1
+                }`)
+  
+  const [review,setReview] = useState(``)
+  const [loading,setLoading] = useState(false);
+
+  useEffect(()=>{
+    prism.highlightAll()
+  },[])
+
+   async function reviewCode(){
+    setLoading(true)
+    try {
+      const response = await axios.post("http://localhost:3000/ai/get-review", {code})
+    setReview(response.data)
+    } catch (error) {
+      setReview("Error fetching review. Please try again.",error);
+    }
+    setLoading(false);
+    
+  }
+
+  return (
+    <>
+    <main>
+      <div className="left">
+        <div className="code">
+          <Editor
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code=> prism.highlight(code, prism.languages.javascript, "javascript")}
+          padding={10}
+          style={{
+            height: "100%",
+            width: "100%",
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+            border: "1px solid #ddd",
+            borderRadius: "5px"
+          }}
+          >
+
+          </Editor>
+        </div>
+        <button onClick={reviewCode} className="review glow-on-hover">Review</button>
+      </div>
+      <div className="right">
+        {
+          loading ? (
+            <div className="loading">Loading...</div>
+          ) : (
+            <Markdown
+        rehypePlugins={[rehypeHighlight]}
+        >{review}</Markdown>
+          )
+        }
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
