be0e3bac2e2e0ff11e33670270b4b2379f07be83
denishuronw21 <notbdenish@gmail.com>
2025-02-26 02:37:25 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const app = require("./src/app")
 
 
 app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
     
 })
