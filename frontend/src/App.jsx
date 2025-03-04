3416f9f19aa12a2eb63739d4bc14534a08233cc7
denishuronw21 <notbdenish@gmail.com>
2025-03-04 13:54:47 +0530

    change title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
    async function reviewCode(){
     setLoading(true)
     try {
-      const response = await axios.post("http://localhost:3000/ai/get-review", {code})
+      const response = await axios.post("https://ai-powered-code-review.onrender.com/ai/get-review", {code})
     setReview(response.data)
     } catch (error) {
       setReview("Error fetching review. Please try again.",error);
